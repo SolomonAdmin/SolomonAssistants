295ea79c1c87d3ee2fa62196d8eaa6da00617f01
Ross Dickinson <rossdickinson@Rosss-MacBook-Pro.local>
2024-08-27 23:16:00 -0400

    removed auth in deployment.

--- app/auth/models_auth.py
+++ app/auth/models_auth.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, EmailStr
+# from pydantic import BaseModel, EmailStr
 
-class UserInDB(BaseModel):
-    id: str
-    email: str
-    # Add other fields as necessary
+# class UserInDB(BaseModel):
+#     id: str
+#     email: str
+#     # Add other fields as necessary
 
-class UserSignUp(BaseModel):
-    email: EmailStr
-    password: str
+# class UserSignUp(BaseModel):
+#     email: EmailStr
+#     password: str
--- app/router.py
+++ app/router.py
@@ -6,11 +6,11 @@ from routers.router_messages import router_messages
 from routers.router_vector_stores import router_vector_stores
 from routers.router_vector_store_files import router_vector_store_files
 from routers.router_files import router_files
-from auth.router_auth import router_auth
+# from auth.router_auth import router_auth
 
 router = SafeAPIRouter(prefix="/api/v2")
 
-router.include_router(router_auth)
+# router.include_router(router_auth)
 router.include_router(router_assistant)
 router.include_router(router_runs)
 router.include_router(router_messages)
