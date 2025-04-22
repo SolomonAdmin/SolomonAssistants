f8cc37c86b979ca13b1f76011f0b46842ad9f780
SolomonAdmin <devops@solomonconsult.com>
2025-04-22 10:45:59 -0400

    changes to make sure websocket is working locally

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -1,6 +1,5 @@
 from typing import Dict, Type
 from .base import BaseTool
-# from .weather.current_temperature import CurrentTemperatureTool
 from .workato.workato_tool import WorkatoAssistantFunctionTool
 from .assistants.create_assistant_tool import CreateAssistantTool
 from .assistants.list_assistants_tool import ListAssistantsTool
@@ -10,7 +9,6 @@ from .assistants.call_agent_tool import CallAgentTool
 from .workato_action_tool.workato_action_tool import WorkatoActionTool
 
 tool_registry: Dict[str, Type[BaseTool]] = {
-    # "get_current_temperature": CurrentTemperatureTool,
     "send_workato_assistant_request": WorkatoAssistantFunctionTool,
     "create_assistant": CreateAssistantTool,
     "list_assistants": ListAssistantsTool,
